5e7b35a85f192ce3c967c3137e0eb7464ef2a966
imtiaz <imtiazuddin018@gmail.com>
2022-02-17 16:39:59 +0600

    Image rounded card

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -3,48 +3,78 @@ package me.imtiaz.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth()
-//                    .padding(top = 50.dp)
-                    .border(5.dp, Color.Magenta)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-//                    .padding(16.dp)
-//                    .width(600.dp)
-//                    .requiredWidth(600.dp)
-            ) {
-                Text(text = "Hello World!", modifier = Modifier
-//                    .border(5.dp, Color.Yellow)
-//                    .padding(5.dp)
-//                    .offset(20.dp, 20.dp)
-//                    .border(10.dp, Color.Black)
-//                    .padding(10.dp)
-
-                    //.offset(0.dp, 20.dp)
-//                    .offset(50.dp, 20.dp)
-                    )
-                Spacer(modifier = Modifier.height(50.dp))
-                Text(text = "Imtiaz")
+            val painter = painterResource(id = R.drawable.image_dumy)
+            val title = "This public red color bus"
+            val description = "This public red color bus"
+            Box(modifier = Modifier.fillMaxWidth(0.5f).padding(16.dp)){
+                ImageCard(painter = painter, contentDescription = description, title = title)
             }
+        }
+    }
+}
 
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box(modifier = Modifier.height(200.dp)) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                 Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                    Text(text = title, style = TextStyle(color = Color.White, fontSize = 16.sp))
+            }
         }
     }
 }
