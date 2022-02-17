90f34034968cf738bf93a41615c2b4ad0453cdf3
imtiaz <imtiazuddin018@gmail.com>
2022-02-17 12:03:24 +0600

    Click event, toast

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -1,41 +1,52 @@
 package me.imtiaz.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
-import me.imtiaz.compose.ui.theme.ComposeandroidTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-            Greeting(name = "Md Imtiaz Uddin")
+            Title()
         }
 
     }
+
+    @Composable
+    fun Title() {
+        val context = LocalContext.current
+        Text(
+            text = "Compose Android",
+            fontSize = 28.sp,
+            fontFamily = FontFamily.Cursive,
+            color = colorResource(id = R.color.teal_700),
+            modifier = Modifier.clickable {
+                Toast.makeText(context, "Title text clicked", Toast.LENGTH_SHORT).show()
+
+            }
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun DefaultPreview() {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Title()
+        }
+    }
+
 }
 
-@Composable
-fun Greeting(name: String) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        Text(text = "Hello $name!")
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeandroidTheme {
-        Greeting("Imtiaz Uddin")
-    }
-}
