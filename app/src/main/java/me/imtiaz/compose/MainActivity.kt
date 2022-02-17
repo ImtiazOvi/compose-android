f8dd79c64f1003cdffe9092ddf8a2e7c4b261616
imtiaz <imtiazuddin018@gmail.com>
2022-02-17 15:59:40 +0600

    Modifires

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -4,9 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
@@ -15,42 +15,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxSize()
-//                    .background(Color.Green),
-//                horizontalAlignment = Alignment.CenterHorizontally,
-//                verticalArrangement = Arrangement.SpaceBetween
-//            ) {
-//                Text("Hello")
-//                Text("World")
-//                Text("Imtiaz")
-//            }
-
-
-            Row(
+            Column(
                 modifier = Modifier
-//                    .fillMaxSize(0.5f)
-//                    .height(300.dp)
-                    .width(300.dp)
-                    .fillMaxHeight(0.7f)
-                    .background(Color.Green),
-                horizontalArrangement =Arrangement.SpaceAround,
-                verticalAlignment = Alignment.CenterVertically
+                    .background(Color.Green)
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth()
+//                    .padding(top = 50.dp)
+                    .border(5.dp, Color.Magenta)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Red)
+                    .padding(10.dp)
+//                    .padding(16.dp)
+//                    .width(600.dp)
+//                    .requiredWidth(600.dp)
             ) {
-                Text("Hello")
-                Text("World")
-                Text("Imtiaz")
+                Text(text = "Hello World!", modifier = Modifier
+//                    .border(5.dp, Color.Yellow)
+//                    .padding(5.dp)
+//                    .offset(20.dp, 20.dp)
+//                    .border(10.dp, Color.Black)
+//                    .padding(10.dp)
+
+                    //.offset(0.dp, 20.dp)
+//                    .offset(50.dp, 20.dp)
+                    )
+                Spacer(modifier = Modifier.height(50.dp))
+                Text(text = "Imtiaz")
             }
-
-
-//
-//            Row {
-//                Text("Hello")
-//                Text("World")
-//            }
-
-
 
         }
     }
