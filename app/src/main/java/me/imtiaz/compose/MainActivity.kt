23fdd984c02edffa64e2805c3ab96fbeff41102b
imtiaz <imtiazuddin018@gmail.com>
2022-02-17 12:30:20 +0600

    Layout Row & Column

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -4,23 +4,66 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Title()
+            UserCard()
+        }
+
+    }
+
+    @Composable
+    fun UserCard() {
+       // Title()
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(12.dp)
+                .border(1.dp, color = Color.Gray)
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_profile),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape)
+            )
+
+            Column {
+                Text(text = stringResource(id = R.string.app_name))
+                Button(onClick = {
+                    // perform button clicked action here
+                }) {
+                    Text(text = "View Profile ")
+                }
+            }
         }
 
     }
@@ -42,11 +85,11 @@ class MainActivity : ComponentActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun DefaultPreview() {
+    fun ImagePreview() {
         Surface(modifier = Modifier.fillMaxSize()) {
             Title()
+            UserCard()
         }
     }
-
 }
 
