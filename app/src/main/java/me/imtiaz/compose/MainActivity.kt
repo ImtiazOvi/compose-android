6420ab0c5b2443549983589622f0cca5119c16ff
imtiaz <imtiazuddin018@gmail.com>
2022-02-22 18:12:45 +0600

    Views working process

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -1,78 +1,126 @@
 package me.imtiaz.compose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+
+@Composable
+private fun OptionsMenu(
+    expanded:Boolean,
+    onDismissRequest:()->Unit
+){
+    val listData = listOf("A","b","c")
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = onDismissRequest
+    ){
+        listData.forEach {
+            DropdownMenuItem(onClick = onDismissRequest) {
+                Text(text = it)
+            }
+        }
+    }
+}
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-
-            Column(Modifier.fillMaxSize()) {
-                val color = remember {
-                    mutableStateOf(Color.Yellow)
+            var dropDownSate by remember { mutableStateOf(false) }
+            OptionsMenu(
+                expanded = dropDownSate,
+                onDismissRequest = {
+                    dropDownSate = false
                 }
+            )
 
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-                    color.value = it
-                }
+            Box(modifier = Modifier.fillMaxSize()) {
 
+                TopAppBar(
+                    title = {
+                        Text(text = "Hello", fontSize = 30.sp)
+                    },
+                    navigationIcon = {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Add Icon",
+                            modifier = Modifier.clickable {
+                                dropDownSate = true
+                            }
+                        )
+                    }
+                )
 
-                Box(
+                FloatingActionButton(
+                    onClick = {
+                        Toast.makeText(applicationContext, "Click", Toast.LENGTH_SHORT).show()
+                    },
                     modifier = Modifier
-                        .background(color = color.value)
-                        .weight(1f)
-                        .fillMaxSize()
+                        .align(Alignment.BottomEnd)
+                        .padding(end = 16.dp, bottom = 16.dp),
+                    backgroundColor = MaterialTheme.colors.primary,
+                    content = {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add Icon"
+                        )
+                    }
                 )
             }
-
-        }
-    }
-
-    @Composable
-    fun ColorBox(
-        modifier: Modifier = Modifier,
-        updateColor: (Color) -> Unit
-    ) {
-
-        val color = remember {
-            mutableStateOf(Color.Yellow)
         }
 
-        Box(modifier = modifier
-            .background(Color.Red)
-            .clickable {
-                updateColor(
 
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f
-                    )
-                )
-
-            }) {
-
-        }
+//            var textFieldState by remember {
+//                mutableStateOf("")
+//            }
+//            Scaffold(
+//                modifier = Modifier.fillMaxSize(),
+//            ) {
+//                Column(
+//                    horizontalAlignment = Alignment.CenterHorizontally,
+//                    verticalArrangement = Arrangement.Center,
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(horizontal = 30.dp)
+//                ) {
+//                    TextField(
+//                        value = textFieldState,
+//                        label = {
+//                                Text(text = "Enter your name")
+//                        },
+//                        onValueChange = {
+//                            textFieldState = it
+//                        },
+//                        singleLine = true,
+//                        modifier = Modifier.fillMaxWidth()
+//                    )
+//                    Spacer(modifier = Modifier.height(16.dp))
+//                    Button(onClick = {
+//                        sc.snackbarHostState.showSnackbar(textFieldState,)
+//                    }) {
+//                        Text(text = "Please greet me")
+//                    }
+//                }
+//            }
     }
 }
+
+
 
 
