f2e66cd066cef5a2ad0d049f7978a893a35c6380
imtiaz <imtiazuddin018@gmail.com>
2022-02-22 17:00:34 +0600

    State basic

--- app/src/main/java/me/imtiaz/compose/MainActivity.kt
+++ app/src/main/java/me/imtiaz/compose/MainActivity.kt
@@ -6,9 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,19 +21,55 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
+            Column(Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(Color.Yellow)
+                }
+
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+
+
+                Box(
+                    modifier = Modifier
+                        .background(color = color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
+
         }
     }
 
     @Composable
-    fun ColorBox(modifier: Modifier = Modifier){
+    fun ColorBox(
+        modifier: Modifier = Modifier,
+        updateColor: (Color) -> Unit
+    ) {
 
-        val color = mutableListOf(Color.Yellow)
+        val color = remember {
+            mutableStateOf(Color.Yellow)
+        }
 
-        Box(modifier = Modifier.background(Color.Yellow)
+        Box(modifier = modifier
+            .background(Color.Red)
             .clickable {
-                
+                updateColor(
 
-            }){
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+
+            }) {
 
         }
     }
